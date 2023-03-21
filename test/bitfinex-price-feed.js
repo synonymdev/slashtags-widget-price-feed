7f6372b5758d6375c87143edb943ee3f6308fc1d
dzdidi <deniszalessky@gmail.com>
2023-03-21 12:48:57 +0100

    test coverage
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

---
+++ test/bitfinex-price-feed.js
@@ -0,0 +1,97 @@
+import { strict as assert } from 'node:assert'
+import BitfinexPriceFeeds from '../src/bitfinex-price-feed.js'
+import fs from 'fs'
+import RAM from 'random-access-memory'
+import nock from 'nock'
+import * as mocks from './mocks/bitfinex-api.js'
+
+const config = JSON.parse(fs.readFileSync('./src/schemas/config.json', 'utf-8'))
+const schema = JSON.parse(fs.readFileSync('./src/schemas/slashfeed.json', 'utf-8'))
+config.feedStorage = new RAM()
+
+describe('Price feed', () => {
+  let feeds
+  before(() => {
+    feeds = new BitfinexPriceFeeds(config, schema)
+    feeds.init()
+  })
+
+  after(async function() {
+    this.timeout(10000)
+    await feeds.feedStorage.close()
+    nock.cleanAll()
+  })
+
+  describe('Live price', () => {
+    before(async function () {
+      this.timeout(10000)
+      nock(mocks.API_HOST).get(mocks.LAST_PATH).reply(200, mocks.LAST_RESPONSE)
+      await feeds._updateLivePrice(mocks.testTicker)
+    })
+
+    it('gets stored in hyperdrive', async () => {
+      let res = await feeds.feedStorage.get(config.driveId, `${mocks.testTicker.base}${mocks.testTicker.quote}-last`)
+
+      assert.equal(res.length, 2)
+      assert(res[0] - (new Date().getTime()) < 0)
+      assert.equal(res[1], feeds._formatPrice(mocks.LAST_RESPONSE[6]))
+    })
+  })
+
+  describe('Day price', () => {
+    before(async () => {
+      nock(mocks.API_HOST).get(mocks.ONE_DAY_PATH).reply(200, mocks.ONE_DAY_RESPONSE)
+      await feeds._updateOneDayHistory(mocks.testTicker)
+    })
+
+    it('gets stored in hyperdrive', async () => {
+      let res = await feeds.feedStorage.get(config.driveId, `${mocks.testTicker.base}${mocks.testTicker.quote}-24h`)
+
+      assert.equal(res.length, 24)
+      const sortedResponse = mocks.ONE_DAY_RESPONSE.sort((a, b) => a[0] - b[0])
+      for (let i = 0; i < res.length; i++) {
+        assert.equal(res[i].length, 2)
+        assert.equal(res[i][0], sortedResponse[i][0])
+        assert.equal(res[i][1], feeds._formatPrice(sortedResponse[i][2]))
+      }
+    })
+  })
+
+  describe('Week price', () => {
+    before(async () => {
+      nock(mocks.API_HOST).get(mocks.ONE_WEEK_PATH_12H).reply(200, mocks.ONE_WEEK_RESPONSE_12H)
+      nock(mocks.API_HOST).get(mocks.ONE_WEEK_PATH_1D).reply(200, mocks.ONE_WEEK_RESPONSE_1D)
+      await feeds._updateOneWeekHistory(mocks.testTicker)
+    })
+
+    it('is stored on hyperdrive as a bi-daily entries for last 7 days', async () => {
+      let res = await feeds.feedStorage.get(config.driveId, `${mocks.testTicker.base}${mocks.testTicker.quote}-7d`)
+
+      assert.equal(res.length, 14)
+      const sortedResponse = mocks.ONE_WEEK_RESPONSE_12H.slice(1, 15).sort((a, b) => a[0] - b[0])
+      for (let i = 0; i < res.length; i++) {
+        assert.equal(res[i][0], sortedResponse[i][0])
+        assert.equal(res[i][1], feeds._formatPrice(sortedResponse[i][2]))
+      }
+    })
+  })
+
+  describe('Month price update', () => {
+    before(async () => {
+      nock(mocks.API_HOST).get(mocks.ONE_WEEK_PATH_12H).reply(200, mocks.ONE_WEEK_RESPONSE_12H)
+      nock(mocks.API_HOST).get(mocks.ONE_WEEK_PATH_1D).reply(200, mocks.ONE_WEEK_RESPONSE_1D)
+      await feeds._updateOneWeekHistory(mocks.testTicker)
+    })
+
+    it('is stored on hyperdrive as a daily entries for last 30 days', async () => {
+      let res = await feeds.feedStorage.get(config.driveId, `${mocks.testTicker.base}${mocks.testTicker.quote}-30d`)
+      assert.equal(res.length, 30)
+
+      const sortedResponse = mocks.ONE_WEEK_RESPONSE_1D.slice(1, 31).sort((a, b) => a[0] - b[0])
+      for (let i = 0; i < res.length; i++) {
+        assert.equal(res[i][0], sortedResponse[i][0])
+        assert.equal(res[i][1], feeds._formatPrice(sortedResponse[i][2]))
+      }
+    })
+  })
+})
